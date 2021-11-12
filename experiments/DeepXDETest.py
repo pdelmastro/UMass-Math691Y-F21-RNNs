540cb56e51d1862de48a70467cbeae6330edaae0
dblander <dblander@umass.edu>
2021-11-12 14:12:07 -0500

    Truing to work with the data... :(

--- experiments/DeepXDETest.py
+++ experiments/DeepXDETest.py
@@ -1,14 +1,14 @@
 """Backend supported: tensorflow.compat.v1, tensorflow, pytorch"""
-# import deepxde as dde
+import deepxde as dde
+'''
 import pandas as pd
 
 us_cases_df = pd.read_csv("US_Covid_Cases.csv")
 cases_series = us_cases_df.loc[:,"cases"]
 print(type(cases_series[0]))
-
-"""
-fname_train = cases_series.head(600)
-fname_test = cases_series.tail(len(cases_series)-600)
+'''
+fname_train = "dataset.train"
+fname_test = "dataset.test"
 data = dde.data.DataSet(
     fname_train=fname_train,
     fname_test=fname_test,
@@ -27,4 +27,3 @@ model.compile("adam", lr=0.001, metrics=["l2 relative error"])
 losshistory, train_state = model.train(epochs=50000)
 
 dde.saveplot(losshistory, train_state, issave=True, isplot=True)
-"""
