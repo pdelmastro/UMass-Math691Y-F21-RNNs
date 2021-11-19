2e2cce8eb1c835b2b3d7bd85e13bc8d8182fbc6a
dblander <dblander@umass.edu>
2021-11-19 13:23:47 -0500

    Playing around with the data

--- experiments/npz_to_txt.py
+++ experiments/npz_to_txt.py
@@ -5,9 +5,7 @@ print(data.files)
 row = data.files
 print(data["y"])
 np.set_printoptions(threshold=np.inf)
-#print(data['arr_0'])
 sys.stdout=open("Lorenz.txt","w")
 for i in row:
-    print("--------------------------")
     print(data[i])
 sys.stdout.close()
